43167bf85b882fdb6d8aeff22bde235f6f285ab4
Edgar S. Hurtado <esanchez@m-lean.com>
2024-03-06 08:05:45 +0100

    Create projec 10 challenge 3

---
+++ p10-challenge-2-storm-viewer/p10-challenge-2-storm-viewer/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  p01-storm-viewer
+//
+//  Created by Edgar Sánchez Hurtado on 5/2/23.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(recommendAppTapped))
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        pictures.sort()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let detailViewController = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            detailViewController.selectedImage = pictures[indexPath.row]
+            detailViewController.title = "Picture \(indexPath.row + 1) of \(pictures.count)"
+            navigationController?.pushViewController(detailViewController, animated: true)
+        }
+    }
+    
+    @objc func recommendAppTapped() {
+        let vc = UIActivityViewController(activityItems: ["Check out this awesome app"], applicationActivities: [])
+        vc.popoverPresentationController?.sourceItem = navigationItem.leftBarButtonItem
+        present(vc, animated: true)
+    }
+
+}
+

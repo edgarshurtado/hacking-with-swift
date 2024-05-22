600f689521175549279343948d06eb78c80896b5
Edgar S. Hurtado <esanchez@m-lean.com>
2024-05-22 09:30:36 +0200

    Complete challenge 2 of project 12

--- p12-challange02-guess-the-flag/p12-challange02-guess-the-flag/ViewController.swift
+++ p12-challange02-guess-the-flag/p12-challange02-guess-the-flag/ViewController.swift
@@ -18,7 +18,9 @@ class ViewController: UIViewController {
     var correctAnswer = 0
     var numberOfGuesses = 0
     let maxNumberOfGuesses = 3
-    
+    var maxScore = 0
+    let defaults = UserDefaults.standard
+
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(showCurrentScore))
@@ -44,11 +46,13 @@ class ViewController: UIViewController {
         button1.layer.borderColor = UIColor.lightGray.cgColor
         button2.layer.borderColor = UIColor.lightGray.cgColor
         button3.layer.borderColor = UIColor.lightGray.cgColor
-        
+
+        maxScore = defaults.integer(forKey: "maxScore")
+
         newGame()
     }
     
-    func askQuestion(something: UIAlertAction! = nil) {
+    func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
         title = "\(countries[correctAnswer].uppercased()). Score: \(score)"
@@ -57,18 +61,38 @@ class ViewController: UIViewController {
         button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
     
-    func newGame(alertAction: UIAlertAction! = nil) {
+    func newGame() {
+        if score > maxScore {
+            defaults.set(score, forKey: "maxScore")
+        }
         score = 0
         numberOfGuesses = 0
         askQuestion()
     }
-    
-    func showEndGameAlertModal(_ action: UIAlertAction) -> Void {
+
+    func endGame() -> Void {
+        if score > maxScore {
+            defaults.set(score, forKey: "maxScore")
+            showNewMaxScoreModal()
+            return
+        }
+
+        showEndGameAlertModal()
+    }
+
+    func showEndGameAlertModal() -> Void {
         let ac = UIAlertController(title: "Final Score", message: "Yoy obtained a final score of: \(score)", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "New game", style: .default, handler: newGame))
+        ac.addAction(UIAlertAction(title: "New game", style: .default, handler: { _ in self.newGame() } ))
         present(ac, animated: true)
     }
-    
+
+    func showNewMaxScoreModal() -> Void {
+        defaults.set(score, forKey: "maxScore")
+        let ac = UIAlertController(title: "New Max Score!", message: "Congratulations. You obtained a new max score of \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "New Game", style: .default, handler: { _ in  self.newGame() }))
+        present(ac, animated: true)
+    }
+
     @IBAction func buttonTapped(_ sender: UIButton) {
         var additionalMessage = ""
         
@@ -87,10 +111,10 @@ class ViewController: UIViewController {
         ac.title = title
         ac.message = additionalMessage + (ac.message ?? "")
         
-        if numberOfGuesses == maxNumberOfGuesses {
-            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: showEndGameAlertModal))
+        if numberOfGuesses >= maxNumberOfGuesses {
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: { _ in self.endGame() }))
         } else {
-            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: { _ in self.askQuestion() }))
         }
         
         present(ac, animated: true)

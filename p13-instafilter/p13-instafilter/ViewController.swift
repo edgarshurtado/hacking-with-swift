489ea9baf4b7dd5557516e666d23c1aba331bcdd
Edgar S. Hurtado <esanchez@m-lean.com>
2024-06-03 08:55:30 +0200

    Layout project 13

--- p13-instafilter/p13-instafilter/ViewController.swift
+++ p13-instafilter/p13-instafilter/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var intensity: UISlider!
+    @IBOutlet var imageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeFilter(_ sender: UIButton) {
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+    }
+    
 }
 

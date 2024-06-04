548ec4bf95792d5570a33981901510955c8ab85a
Edgar S. Hurtado <esanchez@m-lean.com>
2024-06-04 08:33:15 +0200

    Add code to load image from user gallery

--- p13-instafilter/p13-instafilter/ViewController.swift
+++ p13-instafilter/p13-instafilter/ViewController.swift
@@ -7,15 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet var intensity: UISlider!
     @IBOutlet var imageView: UIImageView!
+    var currentImage: UIImage!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "YACIFP"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(importPicture))
     }
+    // MARK: Methods
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+    }
+
+    // MARK: IBActions
 
     @IBAction func changeFilter(_ sender: UIButton) {
     }
@@ -23,5 +40,7 @@ class ViewController: UIViewController {
     @IBAction func save(_ sender: UIButton) {
     }
     
+    @IBAction func intensityChanged(_ sender: UISlider) {
+    }
 }
 

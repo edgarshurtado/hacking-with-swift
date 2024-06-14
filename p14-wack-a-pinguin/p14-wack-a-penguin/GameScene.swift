43a485fad356bb0cb6672cf38657aa8a7424da81
Edgar S. Hurtado <esanchez@m-lean.com>
2024-06-14 08:50:02 +0200

    Start project 14

---
+++ p14-wack-a-pinguin/p14-wack-a-penguin/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  p14-wack-a-pinguin
+//
+//  Created by Edgar Sánchez Hurtado on 14/6/24.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+}
